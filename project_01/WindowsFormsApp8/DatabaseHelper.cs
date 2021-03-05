29687f7672caacf49e989d1796ff9c3b88e8dd83
rookie-3306 <2821537895@qq.com>
2021-03-06 01:13:50 +0800

    project_01的第一个窗口完成!

---
+++ project_01/WindowsFormsApp8/DatabaseHelper.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WindowsFormsApp8
+{
+    class DatabaseHelper
+    {
+        //设置初始的连接字符串
+        static SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SuperMarket_Database"].ConnectionString);
+        /// <summary>
+        /// 获取SQL连接对象
+        /// </summary>
+        /// <returns>SQL连接对象</returns>
+        public static SqlConnection GetSqlConnection()
+        {
+            return conn;
+        }
+
+        #region 各种基本操作
+        public static void OpenGetSqlConnection()
+        {
+            if(conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+
+        public static SqlCommand GetSqlCommand(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            return cmd;
+        }
+
+        public static SqlDataAdapter GetSqlDataAdapter(string sql)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
+            return sda;
+        }
+
+        public static DataSet GetDataSet(string sql)
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter temp_sda = new SqlDataAdapter(sql, conn);
+            temp_sda.Fill(ds, "temp");
+            return ds;
+        }
+
+        public static SqlDataReader GetSqlDataReader(string sql)
+        {
+            SqlCommand cmd = GetSqlCommand(sql);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public static SqlDataReader GetSqlDataReader(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+        public static int GetExecuteSelectSQLLine(string sql)
+        {
+            SqlCommand cmd = GetSqlCommand(sql);
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            return count;
+        }
+
+        public static int GetExecuteSelectSQLLine(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            return count;
+        }
+
+        public static int ExecuteNonQuery(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static int ExecuteNonQuery(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static object ExecuteScalar(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            return cmd.ExecuteScalar();
+        }
+
+        public static object ExecuteScalar(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteScalar();
+        }
+        #endregion
+
+        #region CUDR操作
+        public static SqlDataReader ExecuteSelect(string sql)
+        {
+            SqlDataReader dr = GetSqlDataReader(sql);
+            return dr;
+        }
+        public static SqlDataReader ExecuteSelect(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            SqlDataReader dr = GetSqlDataReader(sql, parameters);
+            return dr;
+        }
+
+        public static int ExecuteUpdate(string sql)
+        {
+            SqlCommand cmd = GetSqlCommand(sql);
+            return cmd.ExecuteNonQuery();
+        }
+        public static int ExecuteUpdate(string sql, SqlParameter[] parameters)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddRange(parameters);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static int ExecuteDelete(string sql)
+        {
+            return ExecuteUpdate(sql);
+        }
+        public static int ExecuteDelete(string sql, SqlParameter[] parameters)
+        {
+            return ExecuteUpdate(sql, parameters);
+        }
+
+        public static int ExecuteInsert(string sql)
+        {
+            return ExecuteUpdate(sql);
+        }
+        public static int ExecuteInsert(string sql, SqlParameter[] parameters)
+        {
+            return ExecuteUpdate(sql, parameters);
+        }
+        #endregion
+    }
+}
--- project_01/WindowsFormsApp8/Form1.cs
+++ project_01/WindowsFormsApp8/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp8
 {
@@ -15,6 +16,89 @@ namespace WindowsFormsApp8
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DatabaseHelper.OpenGetSqlConnection();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = string.Format("exec add_user @UserId='{0}',@UserName='{1}',@UserPwd='{2}',@DepartmentId='{3}'", textBox1.Text,
+                textBox2.Text, textBox3.Text, textBox4.Text);
+            if (DatabaseHelper.ExecuteNonQuery(sql) > 0)
+            {
+                MessageBox.Show("插入成功!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") || textBox1.Text == null)
+            {
+                MessageBox.Show("请输入用户ID！");
+            }
+            else
+            {
+                string sql = string.Format("exec delete_user_by_id @UserId='{0}'", textBox1.Text);
+                if(DatabaseHelper.ExecuteNonQuery(sql) > 0)
+                {
+                    MessageBox.Show("删除成功!");
+                }
+                else
+                {
+                    MessageBox.Show("删除失败,此用户ID不存在!");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") || textBox1.Text == null)
+            {
+                MessageBox.Show("请输入用户ID！");
+            }
+            else
+            {
+                string sql = string.Format("exec update_user_information @UserId='{0}',@NewUserName='{1}',@NewUserPwd='{2}',@NewDepartmentId='{3}'",
+                    textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text);
+                if (DatabaseHelper.ExecuteNonQuery(sql) > 0)
+                {
+                    MessageBox.Show("修改成功!");
+                }
+                else
+                {
+                    MessageBox.Show("修改失败,此用户ID不存在!");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") || textBox1.Text == null)
+            {
+                MessageBox.Show("请输入用户ID！");
+            }
+            else
+            {
+                string sql = string.Format("exec find_user_by_id @UserId='{0}'",textBox1.Text);
+                SqlDataReader dr = DatabaseHelper.GetSqlDataReader(sql);
+                if (dr.HasRows)
+                {
+                    MessageBox.Show("此用户存在!");
+                }
+                else
+                {
+                    MessageBox.Show("此用户不存在!");
+                }
+                dr.Close();
+            }
         }
     }
 }
