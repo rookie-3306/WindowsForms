c369cd6454bd03925db37ac4333ce31df1f6dc66
rookie-3306 <2821537895@qq.com>
2021-03-07 02:36:20 +0800

    project_01的第二个窗口大致完成!

---
+++ project_01/WindowsFormsApp8/Form2.Designer.cs
@@ -0,0 +1,448 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1241, 203);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "客户编号:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(211, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(449, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "*";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 25);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "公司名称:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(480, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "公司全称:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(105, 285);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "负责人:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(236, 285);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "称谓:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(317, 285);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(126, 23);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(480, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "销售员:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(561, 239);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(653, 25);
+            this.textBox3.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(561, 285);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(653, 23);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(1220, 285);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 15);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "*";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(105, 329);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(206, 25);
+            this.textBox5.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 329);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "联系电话:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(414, 329);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(206, 25);
+            this.textBox6.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(354, 329);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 15);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "手机:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(690, 329);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(524, 25);
+            this.textBox7.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(630, 329);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 15);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "传真:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(105, 377);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(1109, 25);
+            this.textBox8.TabIndex = 24;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(24, 380);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(75, 15);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "客户地址:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(105, 420);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(1109, 25);
+            this.textBox9.TabIndex = 26;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(24, 423);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(75, 15);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "开票地址:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(105, 461);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(1109, 25);
+            this.textBox10.TabIndex = 28;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(24, 464);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(75, 15);
+            this.label15.TabIndex = 27;
+            this.label15.Text = "进货地址:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.ForeColor = System.Drawing.Color.Red;
+            this.label16.Location = new System.Drawing.Point(92, 509);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(121, 15);
+            this.label16.TabIndex = 29;
+            this.label16.Text = "注意:*为必填项!";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(654, 502);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 28);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "插入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(771, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 28);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "删除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(888, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 28);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "修改";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1004, 502);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 28);
+            this.button4.TabIndex = 33;
+            this.button4.Text = "取消";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1118, 502);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 28);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "退出";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 544);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ project_01/WindowsFormsApp8/Form2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            DatabaseHelper.OpenGetSqlConnection();
+            LoadDataGridView();
+        }
+
+        private void LoadDataGridView()
+        {
+            string sql = "SELECT * FROM Customer";
+            DataSet ds = new DataSet();
+            DatabaseHelper.GetSqlDataAdapter(sql).Fill(ds, "temp");
+            dataGridView1.DataSource = ds.Tables["temp"];
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO Customer(CustomerID,CustomerSimpleName,CustomerName,Owner,Title,Telephone,MobilePhone,Fax,SalesManId,CustomerAddress,DeliveryAddress,InvoiceAddress,LastDeliveryDate)" +
+                         "values(@CustomerID, @CustomerSimpleName, @CustomerName, @Owner, @Title, @Telephone, @MobilePhone, @Fax, @SalesManId, @CustomerAddress, @DeliveryAddress, @InvoiceAddress, @LastDeliveryDate)";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@CustomerID", textBox1.Text),
+                new SqlParameter("@CustomerSimpleName", textBox2.Text),
+                new SqlParameter("@CustomerName", textBox3.Text),
+                new SqlParameter("@Owner", textBox4.Text),
+                new SqlParameter("@Title", comboBox1.Text),
+                new SqlParameter("@SalesManId", comboBox2.Text),
+                new SqlParameter("@Telephone", textBox5.Text),
+                new SqlParameter("@MobilePhone", textBox6.Text),
+                new SqlParameter("@Fax", textBox7.Text),
+                new SqlParameter("@CustomerAddress", textBox8.Text),
+                new SqlParameter("@DeliveryAddress", textBox9.Text),
+                new SqlParameter("@InvoiceAddress", textBox10.Text),
+                new SqlParameter("@LastDeliveryDate", DateTime.Now.ToString()),
+            };
+            if(DatabaseHelper.ExecuteInsert(sql, parameters) > 0)
+            {
+                MessageBox.Show("插入成功!");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("插入失败!客户ID已经存在!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM Customer Where CustomerID=@CustomerID";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@CustomerID",textBox1.Text),
+            };
+            if(DatabaseHelper.ExecuteNonQuery(sql,parameters) > 0)
+            {
+                MessageBox.Show("删除成功!");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("删除失败!客户ID不存在!");
+            }
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            int currentRowIndex = dataGridView1.CurrentRow.Index;
+            textBox1.Text = dataGridView1.Rows[currentRowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[currentRowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[currentRowIndex].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[currentRowIndex].Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[currentRowIndex].Cells[4].Value.ToString();
+            comboBox2.Text = dataGridView1.Rows[currentRowIndex].Cells[8].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[currentRowIndex].Cells[5].Value.ToString();
+            textBox6.Text = dataGridView1.Rows[currentRowIndex].Cells[6].Value.ToString();
+            textBox7.Text = dataGridView1.Rows[currentRowIndex].Cells[7].Value.ToString();
+            textBox8.Text = dataGridView1.Rows[currentRowIndex].Cells[9].Value.ToString();
+            textBox9.Text = dataGridView1.Rows[currentRowIndex].Cells[10].Value.ToString();
+            textBox10.Text = dataGridView1.Rows[currentRowIndex].Cells[11].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "UPDATE Customer SET CustomerSimpleName=@CustomerSimpleName,CustomerName=@CustomerName,Owner=@Owner,Title=@Title,Telephone=@Telephone,MobilePhone=@MobilePhone,Fax=@Fax,SalesManId=@SalesManId,CustomerAddress=@CustomerAddress,DeliveryAddress=@DeliveryAddress,InvoiceAddress=@InvoiceAddress,LastDeliveryDate=@LastDeliveryDate" +
+                         " WHERE CustomerId = @CustomerId";
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@CustomerID", textBox1.Text),
+                new SqlParameter("@CustomerSimpleName", textBox2.Text),
+                new SqlParameter("@CustomerName", textBox3.Text),
+                new SqlParameter("@Owner", textBox4.Text),
+                new SqlParameter("@Title", comboBox1.Text),
+                new SqlParameter("@SalesManId", comboBox2.Text),
+                new SqlParameter("@Telephone", textBox5.Text),
+                new SqlParameter("@MobilePhone", textBox6.Text),
+                new SqlParameter("@Fax", textBox7.Text),
+                new SqlParameter("@CustomerAddress", textBox8.Text),
+                new SqlParameter("@DeliveryAddress", textBox9.Text),
+                new SqlParameter("@InvoiceAddress", textBox10.Text),
+                new SqlParameter("@LastDeliveryDate", DateTime.Now.ToString()),
+            };
+            if (DatabaseHelper.ExecuteInsert(sql, parameters) > 0)
+            {
+                MessageBox.Show("修改成功!");
+                LoadDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("修改失败!");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
